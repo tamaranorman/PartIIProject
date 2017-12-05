70cc8ceb3627bab66ceda3d53895580117832803
tamaranorman <tamara_norman@hotmail.com>
2017-12-05 11:31:49 +0000

    Added Atom structure and setup bits in git repository

---
+++ src/PrologInterpreter/Atom.java
@@ -0,0 +1,28 @@
+package PrologInterpreter;
+
+public class Atom {
+	final String atomName;
+	
+	public Atom(String s){
+		atomName = s;
+	}
+	
+	@Override
+	public boolean equals(Object o){
+		if (! (o instanceof Atom)) {
+			return false;
+		}
+		Atom a = (Atom) o;
+		if (a.atomName.equals(atomName)){
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public String getAtomName (){
+		return atomName;
+	}
+
+}
