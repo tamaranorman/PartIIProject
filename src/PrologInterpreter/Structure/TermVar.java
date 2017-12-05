f8e81e27b136516db4556635d271340037e08f4e
tamaranorman <tamara_norman@hotmail.com>
2017-12-05 14:23:00 +0000

    Initial structure for TermVar added

--- src/PrologInterpreter/Structure/TermVar.java
+++ src/PrologInterpreter/Structure/TermVar.java
@@ -1,5 +1,29 @@
 package PrologInterpreter.Structure;
 
-public class TermVar {
+public class TermVar extends Term {
+	static int timeStamp;
+	final int varNo;
+	Term instance;
+	
+	public TermVar(){
+		instance = this;
+		varNo = timeStamp++;
+	}
 
+	@Override
+	boolean unify(Term t) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	Term copy() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	Term reset(){
+		instance = this;
+		return this;
+	}
 }
